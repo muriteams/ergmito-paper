8d5dfa629cf3efdc0024940669ce3ee0c7bd9285
George G. Vega Yon <g.vegayon@gmail.com>
2019-04-12 14:57:21 -0700

    Adding table

--- fivenets.R
+++ fivenets.R
@@ -60,7 +60,7 @@ texreg(
     "the Full model (last column of the table) has overall a better fit of the",
     "data. More over, the 95\\% level CI of each covers the true parameters:",
     sprintf(
-      "$\\theta_{edges} \\in [%.2f, %.2f]$; $\\theta_{Homophily} \\in [%.2f, %.2f]$.",
+      "$\\hat\\theta_{edges} \\in [%.2f, %.2f]$; $\\hat\\theta_{Homophily} \\in [%.2f, %.2f]$.",
       ci_11["edges", "2.5 %"], ci_11["edges", "97.5 %"],
       ci_11["nodematch.female", "2.5 %"], ci_11["nodematch.female", "97.5 %"]
       )
