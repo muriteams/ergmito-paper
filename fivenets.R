63981c5358fb3d64b583191ede28c5bca8526c7a
George G. Vega Yon <g.vegayon@gmail.com>
2019-04-12 14:56:12 -0700

    Adding table

--- fivenets.R
+++ fivenets.R
@@ -43,3 +43,29 @@ graphics.off()
 pdf("figures/fivenets_gof.pdf", width = 6, height = 6)
 plot(gof_ergmito(model_11), main = "")
 dev.off()
+
+# Need to generate the confint for printing
+ci_11 <- confint(model_11)
+
+texreg(
+  l  = list(
+    Homopholy    = model_01,
+    Edgecount    = model_10,
+    `Full model` = model_11
+    ),
+  custom.coef.map = list(edges = "Edgecount", "nodematch.female" = "Homophily (on Gender)"),
+  # ci.force = TRUE,
+  caption  = paste(
+    "Fitted ERGMitos using the fivenets dataset. As expected,",
+    "the Full model (last column of the table) has overall a better fit of the",
+    "data. More over, the 95\\% level CI of each covers the true parameters:",
+    sprintf(
+      "$\\theta_{edges} \\in [%.2f, %.2f]$; $\\theta_{Homophily} \\in [%.2f, %.2f]$.",
+      ci_11["edges", "2.5 %"], ci_11["edges", "97.5 %"],
+      ci_11["nodematch.female", "2.5 %"], ci_11["nodematch.female", "97.5 %"]
+      )
+  ),
+  file = "fivenets_fit.tex"
+  )
+
+
